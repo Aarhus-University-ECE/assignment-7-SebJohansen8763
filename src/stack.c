be55b5662797a5e9b9a36498845f2011ac5fa691
SebJohansen8763 <113422792+SebJohansen8763@users.noreply.github.com>
2022-10-11 09:06:01 +0200

    code

--- src/stack.c
+++ src/stack.c
@@ -1,7 +1,15 @@
 #include "stack.h"
+#include <assert.h>
+#define STACKSIZE 10
+
+typedef struct {
+  int data[STACKSIZE +1];
+  int top;
+}stack;
 
 void initialize(stack* s){
-  //implement initialize here
+    /*implement initialize here
+    post condition: s->top = -1;  */
 }
 
 void push(int x, stack* s){
@@ -9,17 +17,18 @@ void push(int x, stack* s){
 }
 
 int pop(stack* s){
-    // implement pop here
+    /* implement pop here
+  assert(s->top>=0);  */
+
   return -1;
 }
 
-bool empty(stack* s)
-{
+bool empty(stack* s){
   //implement empty here
   return false;
 }
 
 bool full(stack* s) {
-    //implement full here
+   /* int top = STACKSIZE -1;??????*/
   return false;
 }
